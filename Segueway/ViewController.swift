76ccb553a057f092b2746c6e8f1cb5cc07eb4a63
黃鈺傑 <soccerss1988@icloud.com>
2019-04-24 20:29:30 +0800

    Initial Commit

---
+++ Segueway/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Segueway
+//
+//  Created by YJ Huang on 2019/4/24.
+//  Copyright © 2019 YJ Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
